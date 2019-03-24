492ee9b9fc8f1a780b58bcd3e8596facf16e9eb8
Vinicius Sampaio <vsampaio@ice.ufjf.br>
2019-03-23 21:48:04 -0300

    salvando home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as M from "materialize-css/dist/js/materialize";
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit() {
   }
